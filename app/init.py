ff3cbda597c88241d65223435a9b32714242f6ef
prerna phadnis <prernaphadnis@Prernas-MacBook-Air.local>
2026-05-16 15:38:54 +0530

    backend code

---
+++ app/init.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+
+    CORS(app)
+
+    from app.routes.expenses import expenses_bp
+
+    app.register_blueprint(expenses_bp)
+
+    return app
